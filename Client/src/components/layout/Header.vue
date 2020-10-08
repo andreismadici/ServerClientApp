443ac9cfc565f61dfabea46b2e0a0db9a2c1d98e
andreismadici <57235361+andreismadici@users.noreply.github.com>
2020-10-08 08:20:52 +0300

    Add files via upload

---
+++ Client/src/components/layout/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+        <h1>Comanda</h1>
+        <button @click="$emit('trimite-comanda')" class="btn">Trimite Comanda</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+   
+
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+
+    .btn {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+
+
+</style>
---
+++ Client/src/components/layout/HeaderAdmin.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="headerAdmin">
+        <h1>Admin Controler</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderAdmin"
+
+}
+</script>
+
+<style scoped>
+    .headerAdmin {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+</style>
